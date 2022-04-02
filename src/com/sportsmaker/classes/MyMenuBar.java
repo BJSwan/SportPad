ef99d89e9bbd9772b391137f6f981066b2f28c5f
Bryton J <brytonjswan@gmail.com>
2022-04-02 13:37:59 -0400

    Added more menu bar buttons

--- src/com/sportsmaker/classes/MyMenuBar.java
+++ src/com/sportsmaker/classes/MyMenuBar.java
@@ -13,8 +13,8 @@ import javax.swing.JPanel;
 
 public class MyMenuBar extends JMenuBar{
 	
-	private JMenu file, edit, option;
-	private JMenuItem button1, button2, button3, button4;
+	private JMenu file, edit, position, equipment, simulate, option;
+	private JMenuItem button1, button2, button3, button4, button5, button6;
 	
 	public MyMenuBar() {
 		
@@ -25,11 +25,13 @@ public class MyMenuBar extends JMenuBar{
 		button2 = new JMenuItem("Save");
 		button1.addActionListener(new saveListener());
 		button3 = new JMenuItem("Save As");
-		button4 = new JMenuItem("Exit");
+		button4 = new JMenuItem("Open");
+		button6 = new JMenuItem("Exit");
 		file.add(button1);
 		file.add(button2);
 		file.add(button3);
 		file.add(button4);
+		file.add(button6);
 		this.add(file);
 		
 		//Adding Edit Menu
@@ -40,6 +42,32 @@ public class MyMenuBar extends JMenuBar{
 		edit.add(button1);
 		edit.add(button2);
 		edit.add(button3);
+		this.add(edit);
+		
+		//Adding Position Menu
+		position = new JMenu("Positions");
+		button1 = new JMenuItem("New Positon");
+		button2 = new JMenuItem("Edit Position");
+		position.add(button1);
+		position.add(button2);
+		this.add(position);
+		
+		//Adding Equipment Menu
+		equipment = new JMenu("Equipment");
+		button1 = new JMenuItem("New Equipment");
+		button2 = new JMenuItem("Edit Equipment");
+		equipment.add(button1);
+		equipment.add(button2);
+		this.add(equipment);
+		
+		//Adding Edit Menu
+		simulate = new JMenu("Simulate");
+		button1 = new JMenuItem("New League");
+		button2 = new JMenuItem("Load league");
+		button3 = new JMenuItem("Ehibition Game");
+		simulate.add(button1);
+		simulate.add(button2);
+		simulate.add(button3);
 		this.add(edit);
 		
 		//Adding Options Menu
@@ -54,7 +82,7 @@ public class MyMenuBar extends JMenuBar{
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			JFrame frame = new JFrame("New Sport Creator");
+			JFrame frame = new JFrame("New Sport");
 			frame.setPreferredSize(new Dimension(500, 500));
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			
